ee2aa04c3843a29ad5c102b5944fd63e66989b01
jmmarao <jmmarao@ciandt.com>
2022-08-18 14:55:15 -0300

    [TASK-1] Decorator coffe shop

---
+++ 0x01/cafeteria/BebidaCafe.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BebidaCafe extends Bebida {
+    @Override
+    public List<String> obterIngredientes() {
+        List<String> list = new ArrayList<>();
+        list.add("cafe");
+        return list;
+    }
+
+    @Override
+    public double obterPreco() {
+        return 5.35;
+    }
+}
