57125c862c0de2eb21a195d633f6bf13a3fc5c7d
jmmarao <jmmarao@ciandt.com>
2022-08-19 16:58:25 -0300

    [TASK-1] Refactor toString space

--- 0x02/impressao/Impressao.java
+++ 0x02/impressao/Impressao.java
@@ -30,6 +30,6 @@ public class Impressao {
                 ", total coloridas: " + paginasColoridas +
                 ", total preto e branco: " + (paginasTotais - paginasColoridas) +
                 ", " + (ehFrenteVerso ? "frente e verso" : "frente") +
-                " . total: R$ " + String.format("%.2f", calcularTotal());
+                ". total: R$ " + String.format("%.2f", calcularTotal());
     }
 }
